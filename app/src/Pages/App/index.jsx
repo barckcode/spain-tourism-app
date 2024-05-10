3534030e431d1af6a057d69a3c7542b9a53e3e2f
barckcode <cristian@helmcode.com>
2024-05-10 13:02:39 +0100

    Clean Template && Added Basic Dashboard

--- app/src/Pages/App/index.jsx
+++ app/src/Pages/App/index.jsx
@@ -1,13 +1,12 @@
 import { useRoutes, BrowserRouter } from "react-router-dom"
 import Home from '../Home'
 import NotFound from '../NotFound'
-import Login from '../Login'
+
 
 
 const AppRoutes = () => {
 	let routes = useRoutes([
 		{ path: '/', element: <Home /> },
-		{ path: '/login', element: <Login /> },
 		{ path: '/*', element: <NotFound /> },
 	])
 
