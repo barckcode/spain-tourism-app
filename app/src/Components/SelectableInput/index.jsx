10c97ea73618da2a58f80f2da7533baf22afb732
barckcode <cristian@helmcode.com>
2024-05-14 10:41:07 +0100

    Updated Tourists Page && Added SelectableInput Component

---
+++ app/src/Components/SelectableInput/index.jsx
@@ -0,0 +1,61 @@
+import { Combobox, ComboboxButton, ComboboxInput, ComboboxOption, ComboboxOptions, Transition } from '@headlessui/react';
+import { CheckIcon, ChevronDownIcon } from '@heroicons/react/20/solid';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+
+
+export default function SelectableInput({ communities, selected, setSelected, query, setQuery }) {
+    const filteredCommunities = query === ''
+        ? communities
+        : communities.filter((community) => community.name.toLowerCase().includes(query.toLowerCase()));
+
+    return (
+        <div className="mx-auto h-52 w-52 pt-20">
+            <Combobox value={selected} onChange={setSelected}>
+                <div className="relative">
+                    <ComboboxInput
+                        className={clsx(
+                            'w-full rounded-lg border-none bg-black/5 py-1.5 pr-8 pl-3 text-sm/6 text-black',
+                            'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-black/25'
+                        )}
+                        displayValue={(community) => community?.name}
+                        onChange={(event) => setQuery(event.target.value)}
+                    />
+                    <ComboboxButton className="group absolute inset-y-0 right-0 px-2.5">
+                        <ChevronDownIcon className="size-4 fill-black/60 group-data-[hover]:fill-black" />
+                    </ComboboxButton>
+                </div>
+                <Transition
+                    leave="transition ease-in duration-100"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                    afterLeave={() => setQuery('')}
+                >
+                    <ComboboxOptions
+                        anchor="bottom"
+                        className="w-[var(--input-width)] rounded-xl border border-black/5 bg-black/5 p-1 [--anchor-gap:var(--spacing-1)] empty:hidden overflow-auto h-20"
+                    >
+                        {filteredCommunities.map((community) => (
+                            <ComboboxOption
+                                key={community.id}
+                                value={community}
+                                className="group flex cursor-default items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]:bg-black/10"
+                            >
+                                <CheckIcon className="invisible size-4 fill-black group-data-[selected]:visible" />
+                                <div className="text-sm/6 text-black">{community.name}</div>
+                            </ComboboxOption>
+                        ))}
+                    </ComboboxOptions>
+                </Transition>
+            </Combobox>
+        </div>
+    );
+}
+
+SelectableInput.propTypes = {
+    communities: PropTypes.array.isRequired,
+    selected: PropTypes.object,
+    setSelected: PropTypes.func.isRequired,
+    query: PropTypes.string.isRequired,
+    setQuery: PropTypes.func.isRequired,
+};
--- app/src/Pages/Tourists/index.jsx
+++ app/src/Pages/Tourists/index.jsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Combobox, ComboboxButton, ComboboxInput, ComboboxOption, ComboboxOptions, Transition } from '@headlessui/react'
-import { CheckIcon, ChevronDownIcon } from '@heroicons/react/20/solid'
-import clsx from 'clsx'
+import SelectableInput from '../../Components/SelectableInput';
 import HistogramChart from "../../Components/HistogramChart"
 import LineChart from '../../Components/LineChart';
 import DinamicTable from '../../Components/DinamicTable';
@@ -20,17 +18,10 @@ const communities = [
 ]
 
 export default function Tourists() {
+    const [selected, setSelected] = useState(communities[0])
+    const [query, setQuery] = useState('')
     const [data, setData] = useState([]);
     const [lineChartData, setLineChartData] = useState([]);
-    const [query, setQuery] = useState('')
-    const [selected, setSelected] = useState(communities[0])
-
-    const filteredCommunities =
-    query === ''
-        ? communities
-        : communities.filter((community) => {
-                return community.name.toLowerCase().includes(query.toLowerCase())
-            })
 
     useEffect(() => {
         const fetchData = async () => {
@@ -97,45 +88,13 @@ export default function Tourists() {
         <>
             <h1 className='font-bold text-center text-2xl' >Comunidades Autónomas con más afluencia de turistas</h1>
             <p className='pt-8 font-bold text-center text-xs' >Datos desde el 10/2015 al 03/2024</p>
-            <div className="mx-auto h-52 w-52 pt-20">
-                <Combobox value={selected} onChange={(value) => setSelected(value)}>
-                    <div className="relative">
-                        <ComboboxInput
-                            className={clsx(
-                            'w-full rounded-lg border-none bg-black/5 py-1.5 pr-8 pl-3 text-sm/6 text-black',
-                            'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-black/25'
-                            )}
-                            displayValue={(community) => community?.name}
-                            onChange={(event) => setQuery(event.target.value)}
-                        />
-                        <ComboboxButton className="group absolute inset-y-0 right-0 px-2.5">
-                            <ChevronDownIcon className="size-4 fill-black/60 group-data-[hover]:fill-black" />
-                        </ComboboxButton>
-                    </div>
-                    <Transition
-                        leave="transition ease-in duration-100"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                        afterLeave={() => setQuery('')}
-                    >
-                        <ComboboxOptions
-                            anchor="bottom"
-                            className="w-[var(--input-width)] rounded-xl border border-black/5 bg-black/5 p-1 [--anchor-gap:var(--spacing-1)] empty:hidden overflow-auto h-20"
-                        >
-                            {filteredCommunities.map((community) => (
-                            <ComboboxOption
-                                key={community.id}
-                                value={community}
-                                className="group flex cursor-default items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]:bg-black/10"
-                            >
-                                <CheckIcon className="invisible size-4 fill-black group-data-[selected]:visible" />
-                                <div className="text-sm/6 text-black">{community.name}</div>
-                            </ComboboxOption>
-                            ))}
-                        </ComboboxOptions>
-                    </Transition>
-                </Combobox>
-            </div>
+            <SelectableInput
+                communities={communities}
+                selected={selected}
+                setSelected={setSelected}
+                query={query}
+                setQuery={setQuery}
+            />
             <h2 className='font-bold text-center text-xl' >Nº total de turistas al año</h2>
             <HistogramChart data={data} />
 
